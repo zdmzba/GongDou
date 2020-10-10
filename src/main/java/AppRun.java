48e84f3c4c253338a53bc376eac86f88337b7575
zhangguhan <zdmzba@163.com>
2020-10-10 16:49:55 +0800

    初始化pom

---
+++ src/main/java/AppRun.java
@@ -0,0 +1,30 @@
+package com.wonders;
+
+import com.wonders.platform.util.SpringContextHolder;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableAsync
+@RestController
+@SpringBootApplication
+@EnableTransactionManagement
+@MapperScan("com.wonders.**.dao")
+@ServletComponentScan
+public class AppRun {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppRun.class, args);
+    }
+
+    @Bean
+    public SpringContextHolder springContextHolder() {
+        return new SpringContextHolder();
+    }
+
+}
